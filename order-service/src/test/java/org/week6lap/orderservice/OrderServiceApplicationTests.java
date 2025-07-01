94e74250019faaddf42a47d1ccc9a05558a25f1d
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-07-01 09:41:06 -0700

    chore(ci): Initial commit -- add starter directory and files for order-service with configurations

---
+++ order-service/src/test/java/org/week6lap/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.week6lap.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
