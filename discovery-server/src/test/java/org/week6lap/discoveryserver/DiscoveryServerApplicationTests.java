ebb310767374f8f295e5ad2626406d803a18a532
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-07-01 05:06:21 -0700

    chore(ci): Initial commit -- add starter directory and files for config-server with configurations

---
+++ discovery-server/src/test/java/org/week6lap/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.week6lap.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
