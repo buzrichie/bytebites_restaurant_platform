d7291c31364e7d75b4e5bc5d5fb294a88f0df923
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-07-01 09:16:06 -0700

    chore(ci): Initial commit -- add starter directory and files for user-service with configurations

---
+++ user-service/src/test/java/org/week6lap/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.week6lap.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
