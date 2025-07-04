c362afbd5c1e3db3280141c7bf19e2cd85d5ee60
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-07-04 10:05:39 -0700

    feat(api-gateway::congig) add security filter chain

---
+++ api-gateway/src/main/java/org/week6lap/apigateway/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package org.week6lap.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex -> ex
+                        .pathMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyExchange().permitAll() // JWT auth handled by filter
+                )
+                .build();
+    }
+}
